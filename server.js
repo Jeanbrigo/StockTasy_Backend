d5a101173221a3d855c59b7decae14066775d094
Jean Brigonnet <jeanbrigo@gmail.com>
2023-04-19 01:47:17 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const { PORT = 3000 } = process.env;
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("./db/db");
+const AuthRouter = require("./controllers/user");
+const NoteRouter = require("./controllers/notes");
+const auth = require("./auth");
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
+
+// ROUTERS
+
+app.get("/", auth, (req, res) => {
+  res.json(req.payload);
+});
+
+app.use("/auth", AuthRouter);
+
+app.use("/note", NoteRouter);
+
+// LISTENER
+
+app.listen(PORT, () => {
+  console.log(`You are listening on PORT ${PORT}`);
+});
